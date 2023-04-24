cc1d840d228e304497084c8adfa7d118e695412a
ZayaES <heavenphrince@gmail.com>
2023-04-24 12:17:29 -0700

    task1 test2 lint compliant(hopefully)

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # creating a file in /tmp
 
 file { 'tmp/school':
-	ensure => absent,
-	mode => '0744',
-	owner => 'www-data',
-	group => 'www-data',
-	content => 'I love Puppet',
+  ensure => 'present',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
