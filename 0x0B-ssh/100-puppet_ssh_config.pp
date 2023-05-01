a2bff6780e624dd0eb43c9f364264a18faefa7a1
ZayaES <75847404+ZayaES@users.noreply.github.com>
2023-05-01 04:41:59 +0100

    Delete 0x0B-ssh directory

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++
@@ -1,8 +0,0 @@
-# configures ssh to connect via private key
-
-file { 'config':
-  path    => '/etc/ssh/ssh_config',
-  ensure  => 'present',
-  mode    => '0744',
-  content => 'SendEnv LANG LC_*\nHashKnownHosts yes\nHostName 100.25.149.245\nUser ubuntu\nIdentityFile ~/.ssh/school\nPreferredAuthentications publickey\nPasswordAuthentication no'
-}
