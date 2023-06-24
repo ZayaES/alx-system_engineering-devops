9b523b9c4d1038ebb117e12cd417dce82b78b001
Zaya Bell <heavenphrince@gmail.com>
2023-06-24 01:49:38 +0000

    task0 test1

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
 # creating a file with puppet 
 
-file { '/tmp':
+file { 'school':
+  ensure  =>     'present',
   path    =>     '/tmp/school',
   mode    =>     '0744',
   owner   =>     'www-data',
   group   =>     'www-data',
-  content =>     'I love Puppet'
+  content =>     'I love Puppet',
 }
