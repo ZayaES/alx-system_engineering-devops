62b83eb9d8b603c63af767bf8038c7b13a5b990e
Zaya Bell <heavenphrince@gmail.com>
2023-06-27 03:01:27 +0000

    task4 try your luck

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+file { '~/.ssh/config':
+  ensure => file,
+  owner  => 'root',
+  group  => 'root',
+  mode   => '0600',
+  content => 'Host your_server
+    HostName <your_server_hostname_or_ip>
+    User <your_username>
+    IdentityFile ~/.ssh/school
+    PreferredAuthentications publickey
+    PasswordAuthentication no',
+}
+
