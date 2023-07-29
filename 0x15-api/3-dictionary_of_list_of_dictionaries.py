29062a0de06ae96da72947f7d3bf80c4208a36cf
Zaya Bell <heavenphrince@gmail.com>
2023-07-28 21:07:00 -0700

    task3 test1

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""exports the json data from api to csv format"""
+
+import json
+import requests
+
+
+if __name__ == "__main__":
+    users = requests.get(
+            'https://jsonplaceholder.typicode.com/users/').json()
+    total = 0
+    completed = 0
+    y = 0
+    data = {}
+    u_todo = requests.get('https://jsonplaceholder.typicode.com/todos/')
+    todo = u_todo.json()
+    test = {}
+    id_count = 1
+
+    for user in users:
+        t_data = []
+        for x in range(0, len(u_todo.json())):
+            task_data = {}
+            if todo[x]['userId'] == id_count:
+                print(user['id'])
+                print(user['username'])
+                print(todo[x]['title'])
+                task_data = {
+                        "username": str(user['username']),
+                        "task": str(todo[x]['title']),
+                        "completed": (todo[x]['completed'])
+                        }
+                t_data.append(task_data)
+        id_count += 1
+        data[user['id']] = t_data
+
+    filepath = "todo_all_employees.json"
+    json_data = json.dumps(data)
+
+    with open(filepath, mode='w') as json_file:
+        json_file.write(json_data)
