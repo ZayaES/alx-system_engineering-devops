b986d52a0f8c151ea1c92bedb07108385f112027
Zaya <heavenphrince@gmail.com>
2023-11-11 11:07:16 -0800

    task0 test2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ gets the number of subscribers for a given subreddit"""
 import requests
-import json
+
 
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers for subreddit"""
 
     if subreddit is None or type(subreddit) is not str:
         return 0
-    data = requests.get('https://www.reddit.com/r/{}/about.json'.format(subreddit),
-            headers={'User-Agent': 'Boss, na me'})
+    data = requests.get('https://www.reddit.com/r/{}/about.json'.format(
+                                                                subreddit),
+                        headers={'User-Agent': 'Boss, na me'})
     data_j = data.json()
     no_subs = data_j.get('data', {}).get('subscribers', 0)
 
