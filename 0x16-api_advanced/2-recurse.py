1af9b5945b5672b33fe7819ed7fa2e4c4656d233
Zaya <heavenphrince@gmail.com>
2023-11-11 14:18:32 -0800

    task2 test0

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""uses recursion to create a list of hot topics in a subreddit"""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """the main function that does the above"""
+    
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    par = {
+            'count' : count,
+            'after' : after,
+            'limit' : 100
+            }
+    headers = {
+            'User-Agent': 'Boss, na me'
+            }
+
+    if subreddit is None or type(subreddit) is not str:
+        return None
+
+    response = requests.get(url,
+                            headers=headers,
+                            params=par,
+                            allow_redirects=False)
+    response_j = response.json()
+    result = response_j['data']
+    after = result['after']
+    count += result['dist']
+    for topic in result['children']:
+        hot_list.append(topic['data']['title'])
+
+    if after is not None:
+        recurse(subreddit, hot_list, after, count)
+    
+    return hot_list
