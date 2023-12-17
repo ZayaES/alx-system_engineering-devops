a88755aaa002b11bf32e7f44b2093e257961d2f7
Zaya <heavenphrince@gmail.com>
2023-12-17 12:32:10 +0100

    work please

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
-# Fix problem of high amount of requests
+# have high amount of request
 
-exec {'replace':
+exec {'search and replace':
   provider => shell,
-  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
   before   => Exec['restart'],
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
 }
 
 exec {'restart':
