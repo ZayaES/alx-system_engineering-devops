b6a89b1be4d087918667c1bf1f4277c54c2a6c43
Zaya <heavenphrince@gmail.com>
2023-12-17 14:00:03 +0100

    work please3

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,14 @@
 # Allow holberton user to use server
 
-exec {'allow user':
+# Fix problem of high amount files opened
+
+exec {'replace-1':
   provider => shell,
-  command  => 'sudo echo "AllowUsers holberton" >> /etc/ssh/ssh_config',
-  before   => Exec['allow user'],
+  command  => 'sudo sed -i "s/nofile 5/nofile 50000/" /etc/security/limits.conf',
+  before   => Exec['replace-2'],
 }
 
-#exec {'replace-2':
-#  provider => shell,
-#  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
-#}
+exec {'replace-2':
+  provider => shell,
+  command  => 'sudo sed -i "s/nofile 4/nofile 40000/" /etc/security/limits.conf',
+}
